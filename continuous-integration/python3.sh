bbb190d80b8830c4a1737ed0b283908e544b1654
Stuart Mumford <stuart@cadair.com>
2015-10-06 16:41:47 +0100

    add some simple data tests to the python3 script

--- continuous-integration/python3.sh
+++ continuous-integration/python3.sh
@@ -5,3 +5,6 @@
 python setup.py install
 
 python setup.py test -P time
+python -c "import sunpy.data"
+python -c "import sunpy.data; sunpy.data.download_sample_data()"
+python -c "import sunpy.data.sample"
