eb4304c5a219d0612b8c8b5716cefc18341c7041
Stuart Mumford <stuart@cadair.com>
2019-01-31 17:08:36 +0000

    Add back pip 19 fix

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
+import os
+import sys
 import itertools
 
 from setuptools.config import read_configuration
 
+sys.path.append(os.path.abspath("."))
 import ah_bootstrap  # noqa
+
 from astropy_helpers.setup_helpers import setup # noqa
 
 # Create an 'all' extra which is all the others combined
