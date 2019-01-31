6794e5dfc844b7a0794ed9484eb2a8ed8e93253b
Stuart Mumford <stuart@cadair.com>
2019-01-31 18:16:51 +0000

    use setuptools_scm

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
-import os
-import sys
 import itertools
 
+from setuptools import setup
 from setuptools.config import read_configuration
-
-sys.path.append(os.path.abspath("."))
-import ah_bootstrap  # noqa
-
-from astropy_helpers.setup_helpers import setup # noqa
 
 # Create an 'all' extra which is all the others combined
 extras = read_configuration("setup.cfg")['options']['extras_require']
 extras['all'] = list(itertools.chain.from_iterable(extras.values()))
 
-setup(extras_require=extras)
+setup(use_scm_version=True, extras_require=extras)
