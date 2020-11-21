7b0bf78b1b3b399aeb91fc026c5a91bc314d7be3
Nabil Freij <nabil.freij@gmail.com>
2020-11-21 13:34:40 +0000

    changes to address text

--- sunpy/tests/self_test.py
+++ sunpy/tests/self_test.py
@@ -91,9 +91,9 @@ def _self_test_args(*, package=None, online=False, online_only=False, figure_onl
 def self_test(*, package=None, online=False, online_only=False, figure_only=False):
     print("\n\n")
     print("Starting sunpy self test...")
-    print("Checking for installed packages:")
+    print("Checking for packages needed to run sunpy:")
 
-    missing = missing_dependencies_by_extra(exclude_extras=("dev", "all", "docs"))
+    missing = missing_dependencies_by_extra(exclude_extras=("asdf", "dask", "dev", "all", "docs"))
     test_missing = missing.pop("tests")
     printed = print_missing_dependencies_report(missing)
 
@@ -102,6 +102,7 @@ def self_test(*, package=None, online=False, online_only=False, figure_only=Fals
 
     if test_missing:
         print("You do not have all the required dependencies installed to run the sunpy test suite.")
+        print("NOTE THIS JUST MEANS THE TESTS WILL NOT RUN, NOT THAT SUNPY MISCONFIGURED.")
         print(
             "If you want to run the sunpy tests install the 'tests' extra with `pip install sunpy[all,tests]`")
         return
