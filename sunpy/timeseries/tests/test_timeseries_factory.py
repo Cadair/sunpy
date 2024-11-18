09dcb1dc2440b2647719adbde03dd3b030d723d5
wmoapl <144060883+wmoapl@users.noreply.github.com>
2024-11-18 17:43:20 -0500

    Htm s3 awareness (#11)
    upgrade to cdflib==1.3.2

--- sunpy/timeseries/tests/test_timeseries_factory.py
+++ sunpy/timeseries/tests/test_timeseries_factory.py
@@ -30,6 +30,7 @@ goes_filepath = get_test_filepath('go1520110607.fits')
 psp_filepath = get_test_filepath('psp_fld_l2_mag_rtn_1min_20200104_v02.cdf')
 swa_filepath = get_test_filepath('solo_L1_swa-pas-mom_20200706_V01.cdf')
 fermi_gbm_filepath = get_test_filepath('gbm.fits')
+hsi_filepath = get_test_filepath('hsi_image_20101016_191218.fits')
 
 
 @pytest.mark.filterwarnings('ignore:Unknown units')
@@ -100,12 +101,8 @@ def test_from_url():
 @pytest.mark.remote_data
 def test_from_uri():
     # Test read on PSP file saved on public sumpy s3 repository.
-    uri = ('s3://data.sunpy.org/sunpy/psp_fld_l2_mag_rtn_1min_20200104_v02.cdf')
+    uri = ('s3://data.sunpy.org/sunpy/v1/psp_fld_l2_mag_rtn_1min_20200104_v02.cdf')
     ts = sunpy.timeseries.TimeSeries(uri, fsspec_kwargs={'anon':True})
-    # cdflib cannot read from s3 anonymously, so it returns an empty list
-    # this is a temporary workaround
-    if len(ts)==0:
-        ts = sunpy.timeseries.TimeSeries(psp_filepath)
     assert isinstance(ts[0], sunpy.timeseries.GenericTimeSeries)
     assert isinstance(ts[1], sunpy.timeseries.GenericTimeSeries)
 
@@ -444,3 +441,7 @@ def test_validate_meta_astropy_header():
     header = hdulist[0].header
     hdulist.close()
     assert sunpy.timeseries.TimeSeries._is_metadata(header)
+
+def test_get_matching_widget():
+    with pytest.raises(NoMatchError, match=""):
+        sunpy.timeseries.TimeSeries(hsi_filepath)
